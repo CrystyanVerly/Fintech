3b763b8461570fc805a2413d8f949802f15fd07e
unknown <crystyanknappverly@gmail.com>
2026-08-07 16:25:37 -0300

    add reative dates

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import DateInput from './DateInput';
+import { useData } from '../Context/DataContext';
 
 const DateRange = () => {
-	const [inicio, setInicio] = React.useState('');
-	const [final, setFinal] = React.useState('');
+	const { inicio, setInicio, final, setFinal } = useData();
 
 	return (
 		<form className="box flex" onSubmit={(e) => e.preventDefault()}>
@@ -12,13 +12,11 @@ const DateRange = () => {
 				value={inicio}
 				onChange={({ target }) => setInicio(target.value)}
 			/>
-			{inicio}
 			<DateInput
 				label="Final"
 				value={final}
 				onChange={({ target }) => setFinal(target.value)}
 			/>
-			{final}
 		</form>
 	);
 };
--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -15,6 +15,10 @@ type IDataContext = {
 	data: IVendas[] | null;
 	loading: boolean;
 	error: Error | null;
+	inicio: string;
+	setInicio: React.Dispatch<React.SetStateAction<string>>;
+	final: string;
+	setFinal: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const DataContext = React.createContext<IDataContext | null>(null);
@@ -25,12 +29,27 @@ export const useData = () => {
 	return context;
 };
 
+function getNDaysAgo(n: number) {
+	const date = new Date();
+	date.setDate(date.getDate() - n);
+	const dd = String(date.getDate()).padStart(2, '0');
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const yyyy = date.getFullYear();
+
+	return `${yyyy}-${mm}-${dd}`;
+}
+
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
-	const URL_API = `https://data.origamid.dev/vendas/`;
+	const [inicio, setInicio] = React.useState(getNDaysAgo(30));
+	const [final, setFinal] = React.useState(getNDaysAgo(0));
+
+	const URL_API = `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`;
 	const { data, loading, error } = useFetch<IVendas[]>(URL_API);
 
 	return (
-		<DataContext.Provider value={{ data, loading, error }}>
+		<DataContext.Provider
+			value={{ data, loading, error, inicio, setInicio, final, setFinal }}
+		>
 			{children}
 		</DataContext.Provider>
 	);
