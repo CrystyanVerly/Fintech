1657735bc81d7d1aa4ba54d139f3e6082f42d60c
unknown <crystyanknappverly@gmail.com>
2026-08-07 20:29:41 -0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,26 @@ import Sidenav from './Components/Sidenav';
 import Header from './Components/Header';
 import { DataContextProvider } from './Context/DataContext';
 import Sales from './Pages/Sales';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sale from './Pages/Sale';
 
 const App = () => {
 	return (
-		<DataContextProvider>
-			<div className="container">
-				<Sidenav />
-				<main>
-					<Header />
-					<Resumo />
-					<Sales />
-				</main>
-			</div>
-		</DataContextProvider>
+		<BrowserRouter>
+			<DataContextProvider>
+				<div className="container">
+					<Sidenav />
+					<main>
+						<Header />
+						<Routes>
+							<Route path="/" element={<Resumo />} />
+							<Route path="/sales" element={<Sales />} />
+							<Route path="/sales/:id" element={<Sale />} />
+						</Routes>
+					</main>
+				</div>
+			</DataContextProvider>
+		</BrowserRouter>
 	);
 };
 
